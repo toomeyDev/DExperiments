0dbdf37e110e56e73bd6b3b21ba7e9c14dae038c
Matthew Toomey <mtoomeydev@gmail.com>
2021-09-10 21:13:03 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# D Experiments
+A collection of programs written in D programming language for learning purposes/experimentation.
+
+## Projects
+-- **number_guessing**:  *A simple command-line based number guessing game.*
---
+++ number_guessing.d
@@ -0,0 +1,22 @@
+import std.stdio;
+void main()
+{
+	writeln("Welcome!");
+	int i = 1000;
+	do{
+		writeln("Guess the number: ");
+		int guess;	
+		readf(" %s", &guess);
+		writeln("The number is ", i, "!");
+		if(guess == i)
+		{
+			writeln("Congratulations, ", guess, " is the correct number!");
+			break;
+		}
+		else
+		{
+			writeln("Wrong guess, try again.");	
+		}
+	}while(true);
+}
+
