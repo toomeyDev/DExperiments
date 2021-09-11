7dbbb6e67804b7ceff0296ee34c2a5e36122113d
Matthew Toomey <mtoomeydev@gmail.com>
2021-09-11 00:23:53 -0400

    first pass at random number generation for numberGuess

--- numberGuess/number_guessing.d
+++ numberGuess/number_guessing.d
@@ -1,15 +1,17 @@
 import std.stdio;
+import std.random;
 void main()
 {
 	// simple number guessing game
 	writeln("Welcome!");
-	int i = 1000;
+	auto rng = Random(26); // create a random number generator from 0-25
+	auto num = uniform(0, 26, rng);
 	do{
 		writeln("Guess the number: ");
 		int guess;	
 		readf(" %s", &guess);
-		writeln("The number is ", i, "!");
-		if(guess == i)
+		writeln("The number is ", num, "!");
+		if(guess == num)
 		{
 			writeln("Congratulations, ", guess, " is the correct number!");
 			break;
